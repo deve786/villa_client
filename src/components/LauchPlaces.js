5db88dc8d17f2bc3a44b5dfcbfb37bc97c9342b4
Developer <{ID}+{username}@users.noreply.github.com>
2024-09-03 13:22:45 +0530

    DataJson

--- src/components/LauchPlaces.js
+++ src/components/LauchPlaces.js
@@ -3,33 +3,54 @@ import React from 'react';
 
 function LauchPlaces() {
 
-    const images1 = [
+    const villa = [
         {
-            src: "/imagTab1.png",
-            alt: "Image 1",
-            id: 1
+            head: {
+                text: "Our Newly Launched Villa in ",
+                span: "Kazhakootam"
+            },
+            subhead: "Indulge in Luxury with our Stunning Collection of Villas",
+            description: {
+                desc1: "Near to Technopark",
+                desc2: "Sea Facing",
+                desc3: "World class Interiors"
+            },
+            bannerImage: '/build1.png',
+            price: '60 Lakh',
+            exterior: [
+                { src: "/imagTab1.png", alt: "Image 1", id: 1 },
+                { src: "/imagTab2.png", alt: "Image 2", id: 2 },
+                { src: "/imagTab3.png", alt: "Image 3", id: 3 },
+                { src: "/imagTab4.png", alt: "Image 4", id: 4 },
+                { src: "/imagTab5.png", alt: "Image 5", id: 5 }
+            ]
         },
         {
-            src: "/imagTab2.png",
-            alt: "Image 2",
-            id: 2
-        },
-        {
-            src: "/imagTab3.png",
-            alt: "Image 3",
-            id: 3
-        },
-        {
-            src: "/imagTab4.png",
-            alt: "Image 4",
-            id: 4
-        },
-        {
-            src: "/imagTab5.png",
-            alt: "Image 5",
-            id: 5
+            head: {
+                text: "Our Newly Launched Villa in ",
+                span: "Thambanoor"
+            },
+            subhead: "Experience the Ultimate in Luxury Living",
+            description: {
+                desc1: "Near to Padamanabha Swamy Temple",
+                desc2: "Sea Facing",
+                desc3: "Exquisite Design"
+            },
+            bannerImage: '/build2.png',
+            price: '70 Lakh',
+            exterior: [
+                { src: "/imagTab11.png", alt: "Image 6", id: 1 },
+                { src: "/imagTab12.png", alt: "Image 7", id: 2 },
+                { src: "/imagTab13.png", alt: "Image 8", id: 3 },
+                { src: "/imagTab14.png", alt: "Image 9", id: 4 },
+                { src: "/imagTab15.png", alt: "Image 10", id: 5 }
+            ]
         }
     ];
+    const villaData = villa[0];
+    const villaData1 = villa[1];
+
+
 
     const images2 = [
         {
@@ -60,33 +81,88 @@ function LauchPlaces() {
     ];
     return (
         <div className='flex flex-col gap-16'>
-            <div className="p-5 md:p-10  flex lg:flex-row flex-col md:flex-row justify-center md:justify-between">
-                {/* <div className="relative flex flex-col md:justify-center  mb-5  w-1/2 ">
+            {
+                villa.map((villaData, index) => 
+                    <div key={index} className="p-5 md:p-10  flex lg:flex-row flex-col md:flex-row justify-center md:justify-between">
 
-                    <div className="lg:absolute inset-0 w-full lg:h-full h-56 -left-28 md:left-0 ">
-                        <Image src="/Rectangle 4.png" layout="fill" alt="Rectangle" className="object-contain" />
 
-                    </div>
+                        <div className='relative flex flex-col md:justify-center  mb-5 w-full md:w-1/2'>
+                            <div className=" lg:absolute inset-0  md:justify-center mb-5">
+                                <Image
+                                    src={villaData.bannerImage}
+                                    layout="fill"
+                                    alt="Rectangle"
+                                    className="object-contain !static md:!w-[100%] !h-[100%]"
+                                />
+                                <div className="absolute  translate-x-1/2 translate-y-0 inset-0 w-32 h-16 md:w-40 md:h-40  z-20 flex items-center justify-center">
+                                    <Image src="/Medallions.png" layout="fill" alt="Medallions" className="object-contain !static md:!w-[100%] md:!h-full !w-60 !h-24" />
+                                    <div className='absolute text-white text-center'>
+                                        <p className='font-semibold md:text-lg text-[12px]'>{villaData.price}</p>
+                                        <p className='md:text-lg text-[10px]'>Onwards</p>
+                                    </div>
+                                </div>
 
-                    <div className="lg:absolute w-full h-full lg:flex hidden relative inset-0 top-32 md:left-[70%] left-1/2 transform -translate-x-1/2 md:w-80 md:h-full md:top-32 z-10">
-                        <Image src="/image.png" layout="fill" alt="Image" className="object-contain" />
-                        <div className="lg:absolute relative lg:flex hidden inset-0 w-32 h-16 top-0 left-0 transform translate-y-0 translate-x-1/2 md:w-40 md:h-40 md:top-0 z-20  flex items-center justify-center">
-                            <div className='text-white z-10 '>
-                                <p className='font-semibold text-lg'>60 Lakh</p>
-                                <p>Onwards</p>
                             </div>
-                            <Image src="/Medallions.png" layout="fill" alt="Medallions" className="object-contain " />
+
                         </div>
 
+
+
+
+
+                        <div className='flex items-center justify-start md:justify-start text-black md:text-left w-full md:w-1/2'>
+                            <div className='flex justify-center flex-col gap-2 md:gap-4 w-full '>
+                                <h1 className="text-3xl md:text-5xl font-semibold">{villaData.head.text}<span className='text-blue-400'>{villaData.head.span}</span>
+                                    {/* Our Newly Launched <br />Villa in <span className='text-blue-400'>Kazhakootam</span> */}
+                                </h1>
+                                <p className='text-sm md:text-lg'>{villaData.subhead}</p>
+                                <ul className='flex flex-col gap-3'>
+                                    {Object.values(villaData.description).map((desc, i) => (
+                                        <li key={i} className='flex gap-1 items-center text-lg font-semibold'>
+                                            <i className="fa-solid fa-circle text-blue-400"></i>{desc}
+                                        </li>
+                                    ))}
+                                </ul>
+
+                                <button className='py-2 text-white px-4 flex w-fit items-center gap-1 text-lg bg-orange-600'>Enquire Now <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                                </svg>
+                                </button>
+                                <div className=''>
+                                    <div className='flex border-b border-gray-200 mb-4'>
+                                        <button className={`py-2 px-4 -mb-px font-medium text-md transition-colors duration-300 text-orange-600 border-orange-600 border-b-2`}>
+                                            Exteriors
+                                        </button>
+                                        <button className={`py-2 px-4 -mb-px font-medium text-md text-slate-500 transition-colors duration-300`}>
+                                            Interiors
+                                        </button>
+                                    </div>
+                                    <div className='flex gap-2  md:flex-nowrap flex-wrap '>
+                                        {/* {villa.exterior.map(image => (
+                    <Image src={image.src} layout="fill" alt={image.alt} className='w-28 h-auto max-w-full !relative !w-24' />
+
+                ))} */}
+                                        {villaData.exterior.map((image) => (
+                                            <Image key={image.id} src={image.src} layout="responsive" width={100} height={100} alt={image.alt} className='w-28 h-auto max-w-full !relative !w-24' />
+                                        ))}
+
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='relative right-20'>
+                                <Image src='/side.png' layout="fill" className='object-contain inset-0 h-24 ' />
+                            </div>
+                        </div>
                     </div>
-                    <div className='absolute left-32 -bottom-10  lg:flex hidden justify-start w-full'><button className="py-1 px-4 w-fit items-center gap-1 border border-orange-600  ">View Demo </button></div>
+                )
+            }
 
-                </div> */}
-
+            {/* <div className='p-5 md:p-10 flex flex-col md:flex-row justify-center md:justify-between'>
                 <div className='relative flex flex-col md:justify-center  mb-5 w-full md:w-1/2'>
                     <div className=" lg:absolute inset-0  md:justify-center mb-5">
                         <Image
-                            src="/build1.png"
+                            src={villaData1.bannerImage}
                             layout="fill"
                             alt="Rectangle"
                             className="object-contain !static md:!w-[100%] !h-[100%]"
@@ -94,102 +170,13 @@ function LauchPlaces() {
                         <div className="absolute  translate-x-1/2 translate-y-0 inset-0 w-32 h-16 md:w-40 md:h-40  z-20 flex items-center justify-center">
                             <Image src="/Medallions.png" layout="fill" alt="Medallions" className="object-contain !static md:!w-[100%] md:!h-full !w-60 !h-24" />
                             <div className='absolute text-white text-center'>
-                                <p className='font-semibold md:text-lg text-[12px]'>60 Lakh</p>
+                                <p className='font-semibold md:text-lg text-[12px]'>{villaData1.price}</p>
                                 <p className='md:text-lg text-[10px]'>Onwards</p>
                             </div>
                         </div>
 
 
-                        {/* <div className='absolute  translate-x-2/3 translate-y-[25%] inset-0  md:justify-center mb-5 w-1/2'>
-                            <Image
-                                src="/Medallions.png"
-                                layout="fill"
-                                alt="Medallions"
-                                className="object-contain !static md:!w-[50%] !w-[75%] "
-                            />
-                            <div className='text-white z-10 '>
-                                <p className='font-semibold text-lg'>75 Lakh</p>
-                                <p>Onwards</p>
-                            </div>
-                        </div> */}
-                    </div>
 
-                </div>
-
-
-
-
-
-                <div className='flex items-center justify-start md:justify-start text-black md:text-left w-full md:w-1/2'>
-                    <div className='flex justify-center flex-col gap-2 md:gap-4 w-full '>
-                        <h1 className="text-3xl md:text-5xl font-semibold">
-                            Our Newly Launched <br />Villa in <span className='text-blue-400'>Kazhakootam</span>
-                        </h1>
-                        <p className='text-sm md:text-lg'>Indulge in Luxury with our Stunning Collection of Villas</p>
-                        <ul className='flex flex-col gap-3'>
-                            <li className='flex gap-1 items-center text-lg font-semibold'><i className="fa-solid fa-circle text-blue-400"></i>Near to Technopark</li>
-                            <li className='flex gap-1 items-center  text-lg font-semibold'><i className="fa-solid fa-circle text-blue-400"></i>Sea Facing</li>
-                            <li className='flex gap-1 items-center  text-lg font-semibold'><i className="fa-solid fa-circle text-blue-400"></i>World class Interiors</li>
-                        </ul>
-                        <button className='py-2 text-white px-4 flex w-fit items-center gap-1 text-lg bg-orange-600'>Enquire Now <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                        </svg>
-                        </button>
-                        <div className=''>
-                            <div className='flex border-b border-gray-200 mb-4'>
-                                <button className={`py-2 px-4 -mb-px font-medium text-md transition-colors duration-300 text-orange-600 border-orange-600 border-b-2`}>
-                                    Exteriors
-                                </button>
-                                <button className={`py-2 px-4 -mb-px font-medium text-md text-slate-500 transition-colors duration-300`}>
-                                    Interiors
-                                </button>
-                            </div>
-                            <div className='flex gap-2  md:flex-nowrap flex-wrap '>
-                                {images1.map(image => (
-                                    <Image src={image.src} layout="fill" alt={image.alt} className='w-28 h-auto max-w-full !relative !w-24' />
-
-                                ))}
-
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className='relative right-20'>
-                        <Image src='/side.png' layout="fill" className='object-contain inset-0 h-24 ' />
-                    </div>
-                </div>
-            </div>
-
-            <div className='p-5 md:p-10 flex flex-col md:flex-row justify-center md:justify-between'>
-                <div className='relative flex flex-col md:justify-center  mb-5 w-full md:w-1/2'>
-                    <div className=" lg:absolute inset-0  md:justify-center mb-5">
-                        <Image
-                            src="/build2.png"
-                            layout="fill"
-                            alt="Rectangle"
-                            className="object-contain !static md:!w-[100%] !h-[100%]"
-                        />
-                        <div className="absolute  translate-x-1/2 translate-y-0 inset-0 w-32 h-16 md:w-40 md:h-40  z-20 flex items-center justify-center">
-                            <Image src="/Medallions.png" layout="fill" alt="Medallions" className="object-contain !static md:!w-[100%] md:!h-full !w-60 !h-24" />
-                            <div className='absolute text-white text-center'>
-                                <p className='font-semibold md:text-lg text-[12px]'>70 Lakh</p>
-                                <p className='md:text-lg text-[10px]'>Onwards</p>
-                            </div>
-                        </div>
-
-
-                        {/* <div className='absolute  translate-x-2/3 translate-y-[25%] inset-0  md:justify-center mb-5 w-1/2'>
-                            <Image
-                                src="/Medallions.png"
-                                layout="fill"
-                                alt="Medallions"
-                                className="object-contain !static md:!w-[50%] !w-[75%] "
-                            />
-                            <div className='text-white z-10 '>
-                                <p className='font-semibold text-lg'>75 Lakh</p>
-                                <p>Onwards</p>
-                            </div>
-                        </div> */}
                     </div>
 
                 </div>
@@ -227,21 +214,17 @@ function LauchPlaces() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
             <div className='bg-gray-100 gap-10 p-5 py-10 md:p-10 flex flex-col md:flex-row justify-center md:justify-between'>
-            <div className='relative flex flex-col md:justify-center  mb-5 w-full md:w-1/2'>
-                    
-                        <Image
-                            src="/ongoing.png"
-                            layout="fill"
-                            alt="Rectangle"
-                            className="object-contain !static md:!w-[100%]  !h-[100%]"
-                        />
-                        
+                <div className='relative flex flex-col md:justify-center  mb-5 w-full md:w-1/2'>
 
-
-                        
+                    <Image
+                        src="/ongoing.png"
+                        layout="fill"
+                        alt="Rectangle"
+                        className="object-contain !static md:!w-[100%]  !h-[100%]"
+                    />
 
                 </div>
 
