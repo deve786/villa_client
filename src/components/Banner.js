5b81ca99523d7638d5bac84dd7aa80727a653d9c
Developer <{ID}+{username}@users.noreply.github.com>
2024-09-04 19:47:14 +0530

    BtnAnimation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -25,12 +25,15 @@ function Banner() {
 
                     <p className='text-center text-2xl'>Indulge in Luxury with our Stunning Collection of Villas</p>
                 </div>
-                <button onClick={scrollToContact} className='py-2 px-4 flex items-center gap-1 transition-all	     bg-orange-600 text-xl hover:bg-orange-700'>
-                    Enquire Now
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <button className='py-2 px-4 flex items-center gap-1 transition-all bg-orange-600 text-xl hover:bg-orange-700 group'>
+                    <span className="transition-transform duration-300 group-hover:-translate-x-[2px]">
+                        Enquire Now
+                    </span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 group-hover:translate-x-[2px] transition-transform duration-300">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
                     </svg>
                 </button>
+
                 <div className='flex flex-col gap-4 absolute right-2'>
                     <Image
                         src="/Group 3.png"
